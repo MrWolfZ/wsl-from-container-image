304e26defdab42054df2a9d7ff21f5fbb0dc3541
Jonathan Ziller <jonathan.ziller@gmail.com>
2025-10-20 20:56:08 +0200

    prefill the query with the `di` function

--- files-for-image/.config/zsh/functions/di.zsh
+++ files-for-image/.config/zsh/functions/di.zsh
@@ -1,7 +1,13 @@
 di() {
+  local query_args=()
+  if [ -n "${1:-}" ]; then
+    query_args=(--query "$1")
+  fi
+
   local dest=$(zoxide query --list --score |
     fzf --layout reverse --info inline --border \
       --preview "eza --all --group-directories-first --header --long --no-user --no-permissions --color=always {2}" \
+      $query_args \
       --no-sort |
     awk '{print $2}')
   [[ -n "$dest" ]] && cd "$dest"
