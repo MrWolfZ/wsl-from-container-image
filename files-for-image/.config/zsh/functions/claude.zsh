a6dc9503b4f4094ceda1aa8bb2676e4b50828d26
Jonathan Ziller <jonathan.ziller@gmail.com>
2025-12-01 10:59:54 +0100

    in claude startup script automatically clear console

--- files-for-image/.config/zsh/functions/claude.zsh
+++ files-for-image/.config/zsh/functions/claude.zsh
@@ -1,6 +1,7 @@
 claude() {
   if [ -f "$PWD/.bin/claude" ]; then
     chmod +x "$PWD/.bin/claude"
+    clear
     "$PWD/.bin/claude" "$@"
   else
     echo "no claude cli entrypoint found relative to working directory"
